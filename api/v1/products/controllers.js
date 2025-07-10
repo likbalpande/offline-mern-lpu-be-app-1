847dada9e1dcecb98021e7b26dac7d520b1525a7
Likhilesh <likbalpande@example.com>
2025-07-10 11:32:53 +0530

    fix: handle validation errors in update product controller

--- api/v1/products/controllers.js
+++ api/v1/products/controllers.js
@@ -75,7 +75,11 @@ const updateProductController = async (req, res) => {
         });
     } catch (err) {
         console.log("🔴 Error in updateProductController -->", err.message);
-        res.status(500).json({ isSuccess: false, message: "Internal Server Error", data: {} });
+        if (err.name === "ValidationError" || err.code == "11000") {
+            res.status(400).json({ isSuccess: false, message: `Err: ${err.message}`, data: {} });
+        } else {
+            res.status(500).json({ isSuccess: false, message: "Internal Server Error", data: {} });
+        }
     }
 };
 
